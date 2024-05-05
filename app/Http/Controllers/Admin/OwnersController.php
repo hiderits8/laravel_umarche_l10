cf832ac38ab74728927ff04ac1636e6f2e4ecabf
hiderits8 <wataru.ts8.hideri@gmail.com>
2024-05-05 21:34:22 +0900

    edit, update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -37,7 +37,7 @@ class OwnersController extends Controller
         // var_dump($q_first);
 
         // dd($e_all, $q_get, $q_first, $c_test);
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
         return view('admin.owners.index', compact('owners'));
     }
 
@@ -75,7 +75,7 @@ class OwnersController extends Controller
      * Display the specified resource.
      */
     public function show(string $id)
-    {
+    {;
         //
     }
 
@@ -84,7 +84,8 @@ class OwnersController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
@@ -92,7 +93,16 @@ class OwnersController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+
+        $owner->save();
+
+        return redirect()
+            ->route('admin.owners.index')
+            ->with('message', 'オーナー情報を更新しました。');
     }
 
     /**
